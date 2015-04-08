d85c932b1ba4dcc541e91750b0b9ed398bbc18d5
Jordan <jordanmcrae@hotmail.com>
2015-04-08 14:55:06 -0400

    Completed assignment. Added 10 users into contact list using faker gem.

--- crm-web/crm.rb
+++ crm-web/crm.rb
@@ -1,6 +1,7 @@
 require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
+require 'faker'
 
 $rolodex = Rolodex.new
 
@@ -16,4 +17,10 @@ end
 
 get '/contacts/new' do
   erb :addcontact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/')
 end
