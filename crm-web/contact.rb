32a74bfbaee5a5f461bdd875ac9e913092864119
Jordan <jordanmcrae@hotmail.com>
2015-04-08 13:51:54 -0400

    Included contact.rb and required it in crm.rb

---
+++ crm-web/contact.rb
@@ -0,0 +1,14 @@
+class Contact
+  attr_accessor :first_name, :last_name, :email, :note, :id
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+
+  def to_s
+    "Contact #{@id}, First name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Note: #{@note}"
+  end
+end
